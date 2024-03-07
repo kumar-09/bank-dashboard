e8658aebb9f905e8bc873ad6ca30eb8dc8e14316
Mahesh Kumar <mahesh.99prajapat@gmail.com>
2024-03-07 16:46:16 +0530

    ADD: frontend-backend(react-django) integrated, form added to add data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import { React, useState, useEffect } from 'react';
+import AccountForm from './accountForm';
 
-function App() {
+const App = () =>{
+
+  const [userData, setUserData] = useState(null);
+
+  const componentDidMount=() =>{
+    // console.log('hello'); 
+  };
+
+  useEffect(()=>{
+    let data;
+    axios.get('http://localhost:8000/api/userdetail/123456789100/').then((res)=>{
+      setUserData(res.data)
+      console.log(res);
+    }).catch((err)=>{
+      console.log('hello world');
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        console.log('Server responded with error:', err.response.data);
+        console.log('Status code:', err.response.status);
+      } else if (err.request) {
+        // The request was made but no response was received
+        console.log('No response received:', err.request);
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error:', err.message);
+      }
+    }); 
+  },[]);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contaienr">
+      <h1>Hello World</h1>
+      <div>
+        <AccountForm/>
+      </div>
+      <div>
+        <h1>User Details</h1>
+        {userData && (
+          <div>
+            <img src={userData.profile_image} alt="User" /> {/* Assuming 'image' is the key containing the image URL */}
+            <p>Name: {userData.name}</p>
+            <p>Email: {userData.email}</p>
+            {/* Add more user details as needed */}
+          </div>
+        )}
+      </div>
     </div>
-  );
+    
+  )
 }
 
 export default App;
